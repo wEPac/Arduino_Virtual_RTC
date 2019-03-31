b2867cb05f38de5fdcf97a4fb340beffd2d87765
wEPac <35659294+wEPac@users.noreply.github.com>
2019-03-31 10:11:01 +0200

    Update VirtualRTC.h

--- src/VirtualRTC.h
+++ src/VirtualRTC.h
@@ -1,6 +1,11 @@
 /*  ============================================================================
  *  Written by Eric Paquot, 03/2019
  *  ============================================================================
+ *  
+ *  A virtual Real Time Clock to manage Unix Time as reference.
+ *  It will provide dat of the week, leap years, moon phases...
+ *  
+ *  ============================================================================
  *
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
@@ -19,36 +24,31 @@
  *
  *  ============================================================================
  */
-
+ 
  
 
-#ifndef _VIRTUAL_RTC
-#define _VIRTUAL_RTC
+#ifndef VirtualRTC_h
+#define VirtualRTC_h
 
 
+
+#include "main.h"
 
 #define  NO_SUMMERTIME    0x00  // NO Summertime
 #define  EU_SUMMERTIME    0x01  // EU rules
 #define  US_SUMMERTIME    0x02  // US rules
-
-struct  DateTime
-{ //                        range   format    example
-  int  y;   // Year:        1970~n  ####      2019
-  byte m;   // Month:       1~12    #~##      1 is January
-  byte d;   // Day:         1~31    #~##      27
-  byte dow; // DayOfWeek:   1~7     #~##      1 is Sunday
-  byte hh;  // hour:        0~23    #~##      8
-  byte mm;  // min:         0~60    #~##      0
-  byte ss;  // s:           0~60    #~##      12
-};
-
-
 
 class VirtualRTC
 {
   public:
     VirtualRTC(byte summertimeRules);
     VirtualRTC();
+
+    ~VirtualRTC();
+
+    VirtualRTC(const VirtualRTC& other)             = delete; // don't copy the object
+    VirtualRTC(VirtualRTC&& other)                  = delete;
+    VirtualRTC& operator=(const VirtualRTC& other)  = delete;
     
     struct  MoonPos
     {
@@ -81,4 +81,4 @@ class VirtualRTC
 };
 
 
-#endif
+#endif  // VirtualRTC_h
